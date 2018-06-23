7bbf200fdcfd73c8076d118c2df01cb39b247097
YWZFrances <1595642024@qq.com>
2018-06-23 19:09:22 +0800

    222

--- php01/p09.php
+++ php01/p09.php
@@ -1,6 +1,19 @@
 <?php
-	
+	header("Content-type: text/html;charset = utf-8");
+
 $cars = array("Volvo","BMW","Toyota");
 echo "I like" . $cars[0] . "," .$cars[1] . " and " . $cars[2] ."22222222";
 
+echo "<br />";
+$color = array("red","blue","green");
+echo count($cars);
+echo "<br />";
+
+$food = array("包子","馒头","花卷");
+$foodlength = count($food);
+for($x=0;$x<$foodlength;$x++){
+	echo $food[$x];
+	echo "<br />";
+}
+echo "<br />";
 ?>
